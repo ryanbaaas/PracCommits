00fc5f842e49a956cc3049ae6b6844ea68995a09
jc427689 <ryan.bastow@my.jcu.edu.au>
2016-08-22 11:30:06 +1000

    Prac 4 Week 5 Completed NumberChecker

--- numberCheck.py
+++ numberCheck.py
@@ -7,8 +7,17 @@ def main():
         numbers.append(value)
 
     min_num = min(numbers)
+    max_num = max(numbers)
+    first_num = numbers[0]
+    last_num = numbers[len(numbers) - 1]
+    avg_num = sum(numbers) // len(numbers)
 
+    print("The First number is: {}".format(first_num))
+    print("The last number is: {}".format(last_num))
     print("The smallest number is: {}".format(min_num))
+    print("The largest number is: {}".format(max_num))
+    print("The average of the numbers is: {}".format(avg_num))
+
 
 
 main()
